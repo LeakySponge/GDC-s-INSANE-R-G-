05d814f16ff838ed06f5a140d9a9a5ea3b699368
LeakySponge <68715897+LeakySponge@users.noreply.github.com>
2025-01-08 08:30:14 -0800

    Updates on Cat Invader
    Sigh

---
+++ Shayan/cat-invaders/main.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+signal game_start
+signal game_end
+signal game_reset
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
